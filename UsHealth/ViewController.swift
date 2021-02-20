0d11d609479594ce220881a5a95b185e4f5ed48e
derekn4 <derekn4@uci.edu>
2021-02-20 14:06:07 -0800

    Initial Commit

---
+++ UsHealth/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UsHealth
+//
+//  Created by Derek Nguyen on 2/20/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
